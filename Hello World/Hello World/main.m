05a381be96f6396beec77731ea891926c12f75be
umblr <leaves369@gmail.com>
2014-12-07 22:40:08 +0900

    Objective-C
    Objective-C

---
+++ Hello World/Hello World/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by UMBLR on 2014/12/07.
+//  Copyright (c) 2014年 raus0. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
