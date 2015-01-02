a3e651833e128916dd0354fa564f6611953f70f8
umblr <leaves369@gmail.com>
2015-01-02 18:01:14 +0900

    Obj-Cで世界に挨拶
    Objective-C

---
+++ Hello World/Hello World/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Hello World
+//
+//  Created by UMBLR on 2014/12/07.
+//  Copyright (c) 2014年 raus0. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+    @autoreleasepool {
+        
+        NSLog(@"Hello, World!");
+        NSLog(@"ようこそ Objective-Cの世界へ!");
+    }
+    return 0;
+}
